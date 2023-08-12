693755622fb5fe08fa61a0cca71f8f339376c744
Coder-Arm <113359996+Coder-Arm@users.noreply.github.com>
2023-08-12 15:46:49 +0530

    push

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [tab, setTab] = useState('Tab 1');
+  function result(e){
+    setTab(e.target.innerText);
+  }
+  console.log(tab)
   return (
     <div>
-        {/* Do not remove the main div */}
+      <li onClick={result}>Tab 1</li>
+      <li onClick={result}>Tab 2</li>
+      <li onClick={result}>Tab 3</li>
+      <span>This is the content for <span>{tab}</span></span>
     </div>
   )
 }
